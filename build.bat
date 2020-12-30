103fe03710cd30a5bf20dc0fe0dac54231e50168
Engin Cilasun <ecilasun@me.com>
2020-12-30 08:50:02 -0500

    Removed unused remark from buildbat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 del dump.txt
 del ROM.coe
+
 riscv64-unknown-elf-gcc.exe -o ROM.elf test/ROM.cpp -Ofast -fno-tree-loop-distribute-patterns -mexplicit-relocs -march=rv32im -mabi=ilp32 -static -mcmodel=medany -fvisibility=hidden -nostartfiles -fPIC -ffunction-sections -fdata-sections -Wl,-e_start -Wl,-melf32lriscv -Wl,--strip-all -Wl,-gc-sections -lc -lgcc -Wl,-Ttest/ROM.lds
-REM clang.exe test/ROM.cpp -target riscv32 -march=rv32imf -isystem"C:/Program Files/LLVM/lib/clang/11.0.0/include"
 riscv64-unknown-elf-objdump.exe -d ROM.elf >> ROMasmdump.txt
 riscv64-unknown-elf-readelf.exe -S ROM.elf >> ROMasmdump.txt
 .\build\release\riscvtool.exe ROM.elf -makerom >> ROM.coe
