c63448e99559a0107ec0be3672b7197680b4e006
Engin Cilasun <ecilasun@me.com>
2021-05-21 06:43:34 -0400

    Fully asynchronous UART input handling using MEI Sample timer interrupt shoots only once now

--- test/ROM_experimental.cpp
+++ test/ROM_experimental.cpp
@@ -29,7 +29,9 @@ const char *FRtoString[]={
 	"FR_NO_FILESYSTEM\r\n"
 };
 
-void runelf()
+uint32_t showtime = 0;
+
+void RunELF()
 {
     // Set up stack pointer and branch to loaded executable's entry point (noreturn)
     // TODO: Can we work out the stack pointer to match the loaded ELF's layout?
@@ -76,7 +78,7 @@ void runelf()
     );
 }
 
-void parseelfheader(SElfFileHeader32 *fheader)
+void ParseELFHeader(SElfFileHeader32 *fheader)
 {
    if (fheader->m_Magic != 0x464C457F)
    {
@@ -122,24 +124,13 @@ void parseelfheader(SElfFileHeader32 *fheader)
          uint8_t *elfsectionpointer = (uint8_t *)sheader.m_Addr;
          pf_lseek(sheader.m_Offset);
          pf_read(elfsectionpointer, sheader.m_Size, &bytesread);
-
-         // DEBUG: dump info about sections to load
-         /*EchoConsole(&names[sheader.m_NameOffset]);
-         EchoConsole("\r\n");
-         EchoConsole(" ");
-         EchoConsoleHex(sheader.m_Addr);
-         EchoConsole(" ");
-         EchoConsoleHex(sheader.m_Size);
-         EchoConsole(" ");
-         EchoConsoleHex(sheader.m_Offset);
-         EchoConsole("\r\n");*/
       }
    }
 
    free(names);
 }
 
-int loadelf(const char *filename)
+int LoadELF(const char *filename)
 {
    FRESULT fr = pf_open(filename);
    if (fr == FR_OK)
@@ -149,7 +140,7 @@ int loadelf(const char *filename)
       // Read header
       SElfFileHeader32 fheader;
       pf_read(&fheader, sizeof(fheader), &bytesread);
-      parseelfheader(&fheader);
+      ParseELFHeader(&fheader);
       return 0;
    }
    else
@@ -196,22 +187,118 @@ void showdir()
 
 }
 
+void ProcessUARTInputAsync()
+{
+   char cmdbuffer[33];
+   int escapemode = 0;
+   while (*IO_UARTRXByteCount)
+   {
+
+      // Step 3: Read the data on UARTRX memory location
+      char checkchar = *IO_UARTRX;
+
+      if (checkchar == 8) // Backspace? (make sure your terminal uses ctrl+h for backspace)
+      {
+         ConsoleCursorStepBack();
+         EchoConsole(" ");
+         ConsoleCursorStepBack();
+      }
+      else if (checkchar == 27) // ESC
+      {
+         escapemode = 1;
+      }
+      else if (checkchar == 13) // Enter?
+      {
+         // First, copy current row
+         ConsoleStringAtRow(cmdbuffer);
+         // Next, send a newline to go down one
+         EchoConsole("\n");
+
+         // Clear the whole screen
+         if ((cmdbuffer[0]='c') && (cmdbuffer[1]=='l') && (cmdbuffer[2]=='s'))
+         {
+            ClearConsole();
+         }
+         else if ((cmdbuffer[0]='h') && (cmdbuffer[1]=='e') && (cmdbuffer[2]=='l') && (cmdbuffer[3]=='p'))
+         {
+            EchoConsole("\r\n");
+            EchoConsole("\r\nMiniTerm version 0.1\r\n(c)2021 Engin Cilasun\r\ndir: list files\r\nload filename: load and run ELF\n\rcls: clear screen\r\nhelp: help screen\r\ntime: elapsed time\r\nrun:branch to entrypoint\r\ndump:dump first 256 bytes of ELF\r\n");
+         }
+         else if ((cmdbuffer[0]='d') && (cmdbuffer[1]=='i') && (cmdbuffer[2]=='r'))
+         {
+            EchoConsole("\r\n");
+            showdir();
+         }
+         else if ((cmdbuffer[0]='l') && (cmdbuffer[1]=='o') && (cmdbuffer[2]=='a') && (cmdbuffer[3]=='d'))
+         {
+            EchoConsole("\r\n");
+            LoadELF(&cmdbuffer[5]); // Skip 'load ' part
+         }
+         else if ((cmdbuffer[0]='d') && (cmdbuffer[1]=='u') && (cmdbuffer[2]=='m') && (cmdbuffer[3]=='p'))
+         {
+               for (uint32_t i=branchaddress;i<branchaddress + 0x100;i+=4)
+               {
+                  EchoInt(i);
+                  EchoUART(":");
+                  EchoInt(*((uint32_t*)i));
+                  EchoUART("\r\n");
+               }
+         }
+         else if ((cmdbuffer[0]='t') && (cmdbuffer[1]=='i') && (cmdbuffer[2]=='m') && (cmdbuffer[3]=='e'))
+            showtime = (showtime+1)%2;
+         else if ((cmdbuffer[0]='r') && (cmdbuffer[1]=='u') && (cmdbuffer[2]=='n'))
+               RunELF();
+      }
+
+      if (escapemode)
+      {
+         ++escapemode;
+         if (escapemode==4)
+         {
+            int cx,cy;
+            GetConsoleCursor(cx, cy);
+            if (checkchar == 'A')
+               SetConsoleCursor(cx, cy-1);
+            if (checkchar == 'B')
+               SetConsoleCursor(cx, cy+1);
+            if (checkchar == 'C')
+               SetConsoleCursor(cx+1, cy);
+            if (checkchar == 'D')
+               SetConsoleCursor(cx-1, cy);
+            escapemode = 0;
+         }
+      }
+      else if (checkchar != 8)
+      {
+         char shortstring[2];
+         shortstring[0]=checkchar;
+         shortstring[1]=0;
+         EchoConsole(shortstring);
+      }
+
+      // Echo characters back to the terminal
+      *IO_UARTTX = checkchar;
+      if (checkchar == 13)
+         *IO_UARTTX = 10; // Echo extra linefeed
+   }
+}
+
 // NOTE: 'interrupt' will save/restore all integer and float registers
 void __attribute__((interrupt("machine"))) trap_handler()
 {
    register uint32_t causedword;
-   //register uint32_t positivedword=0xEFFFFFFF;
-   //register uint32_t fulldword=0xFFFFFFFF;
+   register uint32_t positivedword=0xEFFFFFFF;
+   register uint32_t fulldword=0xFFFFFFFF;
    asm volatile("csrr %0, mcause" : "=r"(causedword));
    if (causedword==7) // Timer
    {
       // Clear timer interrupt (or re-set to a known future?)
       // NOTE: ALWAYS set high part first to avoid false triggers
-      /*asm volatile("csrrw zero, 0x801, %0" :: "r" (positivedword));
-      asm volatile("csrrw zero, 0x800, %0" :: "r" (fulldword));*/
+      asm volatile("csrrw zero, 0x801, %0" :: "r" (positivedword));
+      asm volatile("csrrw zero, 0x800, %0" :: "r" (fulldword));
 
       // Re-set the timer again, one second into the future
-      uint32_t clockhigh, clocklow, tmp;
+      /*uint32_t clockhigh, clocklow, tmp;
       asm volatile(
          "1:\n"
          "rdtimeh %0\n"
@@ -223,16 +310,22 @@ void __attribute__((interrupt("machine"))) trap_handler()
       uint64_t now = (uint64_t(clockhigh)<<32) | clocklow;
       uint64_t future = now + 10000000; // 1 second
       asm volatile("csrrw zero, 0x801, %0" :: "r" ((future&0xFFFFFFFF00000000)>>32));
-      asm volatile("csrrw zero, 0x800, %0" :: "r" (uint32_t(future&0x00000000FFFFFFFF)));
-      EchoUART("TRAP:TIMER\r\n");
+      asm volatile("csrrw zero, 0x800, %0" :: "r" (uint32_t(future&0x00000000FFFFFFFF)));*/
+      EchoUART("TRAP:TMR\r\n");
    }
-   else if (causedword==3) // Breakpoint
-      EchoUART("TRAP:BREAKPOINT\r\n");
+   else if (causedword==3) // Breakpoint - no going back unless EBRK is restored to previous intstruction
+   {
+      // TODO: Respond to debugger stub here?
+      EchoUART("TRAP:BRK\r\n");
+   }
    else if (causedword==11) // External
-      EchoUART("TRAP:EXTERNAL\r\n");
+   {
+      // TODO: Respond to debugger stub here?
+      ProcessUARTInputAsync();
+   }
 }
 
-void SetupTimerInterruptTest()
+void SetupInterruptHandlers()
 {
    // Set up timer interrupt one second into the future
    // NOTE: timecmp register is a custom register on NekoIchi, not memory mapped
@@ -254,11 +347,11 @@ void SetupTimerInterruptTest()
    asm volatile("csrrw zero, 0x800, %0" :: "r" (uint32_t(future&0x00000000FFFFFFFF)));
 
    // Enable machine interrupts
-   int mstatus = 1 << 3;
+   int mstatus = (1 << 3);
    asm volatile("csrrw zero, mstatus,%0" :: "r" (mstatus));
 
-   // Machine timer interrupts enabled
-   int msie = 1 << 7;
+   // Enable machine timer interrupts and machine external interrupts
+   int msie = (1 << 7) | (1 << 11);
    asm volatile("csrrw zero, mie,%0" :: "r" (msie));
 
    // Set trap handler address
@@ -306,16 +399,11 @@ int main()
    sdcardavailable = (pf_mount(&Fs) == FR_OK) ? 1 : 0;
 
    // Load and branch into the boot executable if one is found on the SDCard
-   if (loadelf("BOOT.ELF") != -1)
-       runelf();
-
-   // Set up interrupt test
-   SetupTimerInterruptTest();
+   if (LoadELF("BOOT.ELF") != -1)
+       RunELF();
 
    const unsigned char bgcolor = 0xC0; // BRG -> B=0xC0, R=0x38, G=0x07
    //const unsigned char editbgcolor = 0x00;
-
-   char cmdbuffer[33*25];
 
    // Set output page
    uint32_t page = 0;
@@ -331,111 +419,20 @@ int main()
    GPUSetRegister(6, page);
    GPUSetVideoPage(6);
 
+   // Set up interrupt handlers
+   SetupInterruptHandlers();
+
    // UART communication section
    uint32_t prevmilliseconds = 0;
    uint32_t cursorevenodd = 0;
-   uint32_t toggletime = 0;
    volatile unsigned int gpustate = 0x00000000;
    unsigned int cnt = 0x00000000;
-   int escapemode = 0;
    while(1)
    {
       uint64_t clk = ReadClock();
       uint32_t milliseconds = ClockToMs(clk);
 
-      // Step 1: Read UART FIFO byte count
-      unsigned int bytecount = *IO_UARTRXByteCount;
-
-      // Step 2: Check to see if we have something in the FIFO
-      if (bytecount != 0)
-      {
-         // Step 3: Read the data on UARTRX memory location
-         char checkchar = *IO_UARTRX;
-
-         if (checkchar == 8) // Backspace? (make sure your terminal uses ctrl+h for backspace)
-         {
-            ConsoleCursorStepBack();
-            EchoConsole(" ");
-            ConsoleCursorStepBack();
-         }
-         else if (checkchar == 27) // ESC
-         {
-            escapemode = 1;
-         }
-         else if (checkchar == 13) // Enter?
-         {
-            // First, copy current row
-            ConsoleStringAtRow(cmdbuffer);
-            // Next, send a newline to go down one
-            EchoConsole("\n");
-
-            // Clear the whole screen
-            if ((cmdbuffer[0]='c') && (cmdbuffer[1]=='l') && (cmdbuffer[2]=='s'))
-            {
-               ClearConsole();
-            }
-            else if ((cmdbuffer[0]='h') && (cmdbuffer[1]=='e') && (cmdbuffer[2]=='l') && (cmdbuffer[3]=='p'))
-            {
-               EchoConsole("\r\n");
-               EchoConsole("\r\nMiniTerm version 0.1\r\n(c)2021 Engin Cilasun\r\ndir: list files\r\nload filename: load and run ELF\n\rcls: clear screen\r\nhelp: help screen\r\ntime: elapsed time\r\nrun:branch to entrypoint\r\ndump:dump first 256 bytes of ELF\r\n");
-            }
-            else if ((cmdbuffer[0]='d') && (cmdbuffer[1]=='i') && (cmdbuffer[2]=='r'))
-            {
-               EchoConsole("\r\n");
-               showdir();
-            }
-            else if ((cmdbuffer[0]='l') && (cmdbuffer[1]=='o') && (cmdbuffer[2]=='a') && (cmdbuffer[3]=='d'))
-            {
-               EchoConsole("\r\n");
-               loadelf(&cmdbuffer[5]); // Skip 'load ' part
-            }
-            else if ((cmdbuffer[0]='d') && (cmdbuffer[1]=='u') && (cmdbuffer[2]=='m') && (cmdbuffer[3]=='p'))
-            {
-                for (uint32_t i=branchaddress;i<branchaddress + 0x100;i+=4)
-                {
-                    EchoInt(i);
-                    EchoUART(":");
-                    EchoInt(*((uint32_t*)i));
-                    EchoUART("\r\n");
-                }
-            }
-            else if ((cmdbuffer[0]='t') && (cmdbuffer[1]=='i') && (cmdbuffer[2]=='m') && (cmdbuffer[3]=='e'))
-               toggletime = (toggletime+1)%2;
-            else if ((cmdbuffer[0]='r') && (cmdbuffer[1]=='u') && (cmdbuffer[2]=='n'))
-                runelf();
-         }
-
-         if (escapemode)
-         {
-            ++escapemode;
-            if (escapemode==4)
-            {
-               int cx,cy;
-               GetConsoleCursor(cx, cy);
-               if (checkchar == 'A')
-                  SetConsoleCursor(cx, cy-1);
-               if (checkchar == 'B')
-                  SetConsoleCursor(cx, cy+1);
-               if (checkchar == 'C')
-                  SetConsoleCursor(cx+1, cy);
-               if (checkchar == 'D')
-                  SetConsoleCursor(cx-1, cy);
-               escapemode = 0;
-            }
-         }
-         else if (checkchar != 8)
-         {
-            char shortstring[2];
-            shortstring[0]=checkchar;
-            shortstring[1]=0;
-            EchoConsole(shortstring);
-         }
-
-         // Echo characters back to the terminal
-         *IO_UARTTX = checkchar;
-         if (checkchar == 13)
-            *IO_UARTTX = 10; // Echo extra linefeed
-      }
+      // Hardware interrupt driven input processing happens async to this code
 
       if (gpustate == cnt) // GPU work complete, push more
       {
@@ -459,7 +456,7 @@ int main()
             PrintDMA(cx*8, cy*8, "_");
          }
 
-         if (toggletime)
+         if (showtime)
          {
             uint32_t hours, minutes, seconds;
             ClockMsToHMS(milliseconds, hours,minutes,seconds);
