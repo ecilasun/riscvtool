826ba0171995b2f5203b8808a400599f8ba78637
Engin Cilasun <ecilasun@me.com>
2021-10-28 14:43:51 -0700

    Minimal ROM for e32 Fixed stack to live in SRAM Added UART library Added UART echo back Updated gitignore

--- e32/ROMs/ROMe32/rom.cpp
+++ e32/ROMs/ROMe32/rom.cpp
@@ -9,12 +9,23 @@
 #include "rvcrt0.h"
 
 #include "core.h"
+#include "uart.h"
 
 int main()
 {
+   UARTWrite("E_32\n(c)Engin Cilasun 2022\n");
+
    while(1)
    {
-      // TODO:
+      // Echo back incoming bytes
+      if (*IO_UARTRXByteAvailable)
+      {
+         // Read incoming character
+         uint8_t incoming = *IO_UARTRXTX;
+         // Write back to UART
+         UARTFlush();
+         *IO_UARTRXTX = incoming;
+      }
    }
 
    return 0;
