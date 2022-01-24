c0c47ebd83f0adaa242e773073d0a55e5d85e279
Engin Cilasun <ecilasun@me.com>
2022-01-23 16:32:26 -0800

    Added more of the task code Added missing stat structure

--- e32/SDK/task.c
+++ e32/SDK/task.c
@@ -1,16 +1,30 @@
+#include "core.h"
 #include "task.h"
 
 volatile uint32_t *taskcount = (volatile uint32_t *)0x80001F00;
+volatile uint32_t *currenttask = (volatile uint32_t *)0x80001F04;
 volatile struct STaskContext *tasks = (volatile struct STaskContext *)0x80001F08;
 
-void AddTask(taskfunc _task)
+void InitTasks()
+{
+    *taskcount = 0;
+    *currenttask = 0;
+}
+
+void AddTask(taskfunc _task, uint32_t _stackpointer, uint32_t _interval)
 {
     uint32_t prevcount = (*taskcount);
 
-    // TODO: insert the task first
+    // Insert the task first
+    tasks[prevcount].PC = (uint32_t)_task;
+    tasks[prevcount].regs[2] = _stackpointer; // Stack pointer
+    tasks[prevcount].regs[8] = _stackpointer; // Frame pointer
+    tasks[prevcount].interval = _interval;    // Execution interval
 
-    // Finally, increment the task count
-    // TODO: This needs to stop timer interrupts
-    // and then resume them to avoid wrong count.
+    // Stop timer interrupts during this
+    swap_csr(mie, MIP_MSIP | MIP_MEIP);
+    // Increment the task count
     (*taskcount) = prevcount+1;
+    // Resume timer interrupts
+    swap_csr(mie, MIP_MSIP | MIP_MEIP | MIP_MTIP);
 }
