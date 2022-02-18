3ec0ece429797fb66e539d766b4133e8d13cb73e
Engin Cilasun <ecilasun@me.com>
2022-02-17 22:24:15 -0800

    Changed multicore rt test threading order

--- e32d/ROMs/BOOT/rom.cpp
+++ e32d/ROMs/BOOT/rom.cpp
@@ -321,8 +321,8 @@ vec3 cast_ray(
 
 void render(int hartid, Sphere* spheres, int nb_spheres, Light* lights, int nb_lights) {
   const float fov  = M_PI/3.;
-  for (int j = hartid; j<graphics_height; j+=numharts) { // actual rendering loop
-    for (int i = 0; i<graphics_width; i++) {
+  for (int j = (hartid/2); j<graphics_height; j+=2) { // actual rendering loop
+    for (int i = (hartid%2); i<graphics_width; i+=2) { // interleave horizontal to keep close in cache to avoid excessive trashing
       float dir_x =  (i + 0.5) - graphics_width/2.;
       float dir_y = -(j + 0.5) + graphics_height/2.; // this flips the image.
       float dir_z = -graphics_height/(2.*tan(fov/2.));
