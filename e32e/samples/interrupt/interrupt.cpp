e1cdcf9f3d11acaa5cdabc7c9b32b650042d0b9a
Engin Cilasun <ecilasun@me.com>
2022-04-30 18:53:54 -0700

    Updated interrupt sample to use all cores

--- e32e/samples/interrupt/interrupt.cpp
+++ e32e/samples/interrupt/interrupt.cpp
@@ -12,22 +12,26 @@ uint32_t mytimerthing(const uint32_t hartID)
 
 int main()
 {
-	// Install the TISR on HART#1 & HART#2
+	// Install the TISR on all HARTs
+	InstallTimerISR(1, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*3);
+	InstallTimerISR(2, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*4);
+	InstallTimerISR(3, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*5);
+	InstallTimerISR(4, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*6);
+	InstallTimerISR(5, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*7);
+	InstallTimerISR(6, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*8);
+	InstallTimerISR(7, mytimerthing, HUNDRED_MILLISECONDS_IN_TICKS*9);
 
-	// HART#1 goes at one second intervals
-	InstallTimerISR(1, mytimerthing, ONE_SECOND_IN_TICKS);
+	// Wait until all HARTs clear in response
+	UARTWrite("Waiting for all HARTs\n");
+	while(HARTMAILBOX[1] != 0x0) asm volatile("nop;");
+	while(HARTMAILBOX[2] != 0x0) asm volatile("nop;");
+	while(HARTMAILBOX[3] != 0x0) asm volatile("nop;");
+	while(HARTMAILBOX[4] != 0x0) asm volatile("nop;");
+	while(HARTMAILBOX[5] != 0x0) asm volatile("nop;");
+	while(HARTMAILBOX[6] != 0x0) asm volatile("nop;");
+	while(HARTMAILBOX[7] != 0x0) asm volatile("nop;");
 
-	// HART#2 goes at half second intervals
-	InstallTimerISR(2, mytimerthing, ONE_SECOND_IN_TICKS/2);
-
-	// Wait until HART#1&HART#2 clear in response
-	UARTWrite("Waiting for HART#1\n");
-	while(HARTMAILBOX[1] != 0x0)
-		asm volatile("nop;");
-	while(HARTMAILBOX[2] != 0x0)
-		asm volatile("nop;");
-
-	UARTWrite("HART#1 acknowledged TISR installation, HART#0 sleeping.\n");
+	UARTWrite("All HARTs acknowledged TISR installation, HART#0 going to sleep.\n");
 	while(1)
 	{
 		// Nothing to do here
