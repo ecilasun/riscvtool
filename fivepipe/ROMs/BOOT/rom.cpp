6bbf8a92741498945fe4dd4d378fbdfb9b56a520
Engin Cilasun <ecilasun@me.com>
2022-11-02 22:43:08 -0700

    Added joystick sample

--- fivepipe/ROMs/BOOT/rom.cpp
+++ fivepipe/ROMs/BOOT/rom.cpp
@@ -218,18 +218,6 @@ int main()
 	if (mountattempt!=FR_OK)
 		UARTWrite(FRtoString[mountattempt]);
 
-	// Set low bit RGB color palette
-	/*uint8_t target = 0;
-	for (int b=0;b<4;++b)
-	for (int g=0;g<8;++g)
-		for (int r=0;r<8;++r)
-		GPUSetPal(target++, MAKECOLORRGB24(r*32, g*32, b*64));*/
-
-	// Set scan-out address
-	// One can allocate some memory aligned to 64 bytes and pass
-	// the pointer to this function to set that memory to be the display scan-out
-	//GPUSetVPage((uint32_t)VRAM); // This is already the default
-
 	// Only on first core
 	uint32_t hartid = read_csr(mhartid);
 	if (hartid == 0)
@@ -250,29 +238,3 @@ int main()
 
 	return 0;
 }
-
-/*
-	uint32_t prevVoltage = 0xC004CAFE;
-	uint32_t voltage = 0x00000000;
-	uint32_t x = 0;
-
-		// While we're awake, also run some voltage measurements
-		voltage = (voltage + *XADCPORT)>>1;
-		if (prevVoltage != voltage)
-		{
-			// Clear the line ahead
-			for (int k=0;k<240;++k)
-				VRAMBYTES[(x+1)%320 + k*320] = 0xFF;
-			// Show voltage value
-			uint32_t y = (voltage/273)%240;
-			VRAMBYTES[x + y*320] = 0x0F;
-			if (x==320)
-			{
-				// Flush data cache so we can see what's happening (entire cache, switch to use one with only one cache page)
-				asm volatile( ".word 0xFC000073;");
-			}
-			// Next column
-			x = (x+1)%320;
-			prevVoltage = voltage;
-		}
-*/
---
+++ fivepipe/samples/joystick/joystick.cpp
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#include "core.h"
+#include "uart.h"
+#include "gpu.h"
+#include "xadc.h"
+
+int main( int argc, char **argv )
+{
+	uint32_t prevVoltage = 0xC004CAFE;
+	uint32_t voltage = 0x00000000;
+	uint32_t x = 0;
+
+    while (1)
+    {
+        voltage = (voltage + *XADCPORT)>>1;
+        if (prevVoltage != voltage)
+        {
+            // Clear the line ahead
+            for (int k=0;k<240;++k)
+                VRAMBYTES[(x+1)%320 + k*320] = 0xFF;
+
+            // Show voltage value
+            uint32_t y = (voltage/273)%240;
+            VRAMBYTES[x + y*320] = 0x0F;
+
+            // Flush data cache at last pixel so we can see a coherent image
+            if (x==320)
+                asm volatile( ".word 0xFC000073;");
+
+            // Next column
+            x = (x+1)%320;
+            prevVoltage = voltage;
+        }
+    }
+
+    return 0;
+}
