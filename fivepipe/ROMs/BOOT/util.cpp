c0c3694959ce88e0e1566d3ad8b14cfd71d425f1
Engin Cilasun <ecilasun@me.com>
2022-11-12 18:06:32 -0800

    Faster loading of ELF binaries

--- fivepipe/ROMs/BOOT/util.cpp
+++ fivepipe/ROMs/BOOT/util.cpp
@@ -1,5 +1,6 @@
 
 #include <cstdlib>
+#include <string.h>
 
 #include "core.h"
 #include "fat32/ff.h"
@@ -16,60 +17,41 @@ void ParseELFHeaderAndLoadSections(FIL *fp, SElfFileHeader32 *fheader, uint32_t 
 	}
 
 	jumptarget = fheader->m_Entry;
+	UINT bytesread = 0;
 
-	// Read program header
-	SElfProgramHeader32 pheader;
-	f_lseek(fp, fheader->m_PHOff);
-	UINT bytesread;
-	f_read(fp, &pheader, sizeof(SElfProgramHeader32), &bytesread);
-
-	// Read string table section header
-	unsigned int stringtableindex = fheader->m_SHStrndx;
-	SElfSectionHeader32 stringtablesection;
-	f_lseek(fp, fheader->m_SHOff+fheader->m_SHEntSize*stringtableindex);
-	f_read(fp, &stringtablesection, sizeof(SElfSectionHeader32), &bytesread);
-
-	// Allocate memory for and read string table
-	char *names = (char *)malloc(stringtablesection.m_Size);
-	f_lseek(fp, stringtablesection.m_Offset);
-	f_read(fp, names, stringtablesection.m_Size, &bytesread);
-
-	// Load all resident sections
-	for(unsigned short i=0; i<fheader->m_SHNum; ++i)
+	// Read program headers
+	for (uint32_t i=0; i<fheader->m_PHNum; ++i)
 	{
-		// Seek-load section headers as needed
-		SElfSectionHeader32 sheader;
-		f_lseek(fp, fheader->m_SHOff+fheader->m_SHEntSize*i);
-		f_read(fp, &sheader, sizeof(SElfSectionHeader32), &bytesread);
+		SElfProgramHeader32 pheader;
+		f_lseek(fp, fheader->m_PHOff + fheader->m_PHEntSize*i);
+		f_read(fp, &pheader, sizeof(SElfProgramHeader32), &bytesread);
 
-		// If this is a section worth loading or allocating...
-		if ((sheader.m_Flags & 0x00000007) && sheader.m_Size != 0)
+		UARTWrite("o: 0x");
+		UARTWriteHex(pheader.m_Offset);
+		UARTWrite(" pa: 0x");
+		UARTWriteHex(pheader.m_PAddr);
+		UARTWrite(" fs:0x");
+		UARTWriteHex(pheader.m_FileSz);
+		UARTWrite(" ms:0x");
+		UARTWriteHex(pheader.m_MemSz);
+		UARTWrite(" f:0x");
+		UARTWriteHex(pheader.m_Flags);
+		UARTWrite(" t:0x");
+		UARTWriteHex(pheader.m_Type);
+		UARTWrite("\n");
+
+		// Something here
+		if (pheader.m_MemSz != 0)
 		{
-			uint8_t *elfsectionpointer = (uint8_t *)sheader.m_Addr;
-
-			UARTWrite("addr: 0x");
-			UARTWriteHex(sheader.m_Addr);
-			UARTWrite(", size: 0x");
-			UARTWriteHex(sheader.m_Size);
-			UARTWrite(", type: 0x");
-			UARTWriteHex(sheader.m_Type);
-			UARTWrite("\n");
-
-			if (sheader.m_Type == 0x00000008) // .bss/.tbss has only SHT_NOBITS set
-			{
-				// ...zero initialize memory.
-				__builtin_memset(elfsectionpointer, 0x0, sheader.m_Size);
-			}
-			else
-			{
-				// ...load in place.
-				f_lseek(fp, sheader.m_Offset);
-				f_read(fp, elfsectionpointer, sheader.m_Size, &bytesread);
-			}
+			uint8_t *memaddr = (uint8_t *)pheader.m_PAddr;
+			// Initialize the memory range at target physical address
+			// This can be larger than the loaded size
+			memset(memaddr, 0x0, pheader.m_MemSz);
+			// Load the binary
+			f_lseek(fp, pheader.m_Offset);
+			f_read(fp, memaddr, pheader.m_FileSz, &bytesread);
 		}
 	}
-
-	free(names);
 }
 
 void LoadExecutable(const char *filename, const bool run)
