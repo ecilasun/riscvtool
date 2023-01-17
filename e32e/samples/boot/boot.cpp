cb21e738de43b5607803665012a72beaba903dce
Engin Cilasun <ecilasun@me.com>
2023-01-17 15:10:23 -0800

    Added experimental boot executable

---
+++ e32e/samples/boot/boot.cpp
@@ -0,0 +1,21 @@
+#include "core.h"
+#include "uart.h"
+#include "ps2.h"
+#include <stdlib.h>
+
+int main()
+{
+	UARTWrite("Experimental boot ROM\nCurrent test: raw keyboard input\n");
+
+	while(1)
+	{
+		if (*PS2KEYBOARDDATAAVAIL)
+		{
+			uint32_t code = (*PS2KEYBOARDDATA)&0xFF;
+			UARTWriteHexByte(code);
+			UARTWrite("\n");
+		}
+	};
+
+    return 0;
+}
