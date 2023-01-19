a27c295d31465f145d40a4a50695a8ec0d97429d
Engin Cilasun <ecilasun@me.com>
2023-01-19 13:26:44 -0800

    Updated DMA demo with missing D$ flush

--- e32e/samples/dmatest/dmatest.cpp
+++ e32e/samples/dmatest/dmatest.cpp
@@ -25,6 +25,11 @@ int main()
 	for (uint32_t i=0;i<W*H;++i)
 		bufferA[i] = i;
 
+	// DMA operatins work directly on memory.
+	// Therefore, we need to insert a cache flush here so that
+	// the writes to buffer A are all written back to RAM.
+	CFLUSH_D_L1;
+
 	// Kick a DMA copy to move buffer A contents onto buffer B
 	const uint32_t blockCountInMultiplesOf16bytes = (W*H)/16;
 	UARTWrite("Initiating copy of ");
@@ -32,6 +37,8 @@ int main()
 	UARTWrite(" blocks\n");
 	DMACopyBlocks((uint32_t)bufferA, (uint32_t)bufferB, blockCountInMultiplesOf16bytes);
 
+	// This is not necessary, but used here as an indicator of copy being asynchronous
+	// and that we can still work while it's running.
 	while (DMAPending())
 		UARTWrite(".");
 
