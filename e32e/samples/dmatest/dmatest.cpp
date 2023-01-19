cd9aa3d1f15832004dbab5658680bee3743941ee
Engin Cilasun <ecilasun@me.com>
2023-01-19 10:52:17 -0800

    updated dma test

--- e32e/samples/dmatest/dmatest.cpp
+++ e32e/samples/dmatest/dmatest.cpp
@@ -1,12 +1,17 @@
 #include "core.h"
 #include "gpu.h"
 #include "dma.h"
+#include "uart.h"
 
 int main()
 {
+	UARTWrite("DMA test\n");
+
 	const uint32_t W = 320;
 	const uint32_t H = 240;
 	const uint32_t mode = VIDEOMODE_320PALETTED;
+
+	UARTWrite("Preparing buffers\n");
 
 	// Create source and target buffers (using GPU functions to get aligned buffer addresses)
 	uint8_t *bufferA = GPUAllocateBuffer(W*H);
@@ -21,13 +26,10 @@ int main()
 		bufferA[i] = i;
 
 	// Kick a DMA copy to move buffer A contents onto buffer B
+	UARTWrite("Initiating copy\n");
 	const uint32_t blockCountInMultiplesOf16bytes = (W*H)/16;
 	DMACopyBlocks((uint32_t)bufferB, (uint32_t)bufferA, blockCountInMultiplesOf16bytes);
 
-	// Halt the CPU
-	while(1) {
-		asm volatile("wfi;");
-	} while (1);
-
+	UARTWrite("Copy is now done or is in flight\n");
 	return 0;
 }
