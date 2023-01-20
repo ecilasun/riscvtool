a195d0678a3925f2ce1b1b4570718408e58f6b91
Engin Cilasun <ecilasun@me.com>
2023-01-19 19:42:42 -0800

    Large offscreen image blit added to dmatest

--- e32e/samples/dmatest/dmatest.cpp
+++ e32e/samples/dmatest/dmatest.cpp
@@ -14,16 +14,17 @@ int main()
 	UARTWrite("Preparing buffers\n");
 
 	// Create source and target buffers (using GPU functions to get aligned buffer addresses)
-	uint8_t *bufferA = GPUAllocateBuffer(W*H);
 	uint8_t *bufferB = GPUAllocateBuffer(W*H);
+	uint8_t *bufferA = GPUAllocateBuffer(W*H*2);
 
 	// Set buffer B as output
 	GPUSetVPage((uint32_t)bufferB);
 	GPUSetVMode(MAKEVMODEINFO(mode, VIDEOOUT_ENABLED)); // Mode 0, video on
 
 	// Fill buffer A with some data
-	for (uint32_t i=0;i<W*H;++i)
-		bufferA[i] = i;
+	for (uint32_t y=0;y<H*2;++y)
+		for (uint32_t x=0;x<W;++x)
+			bufferA[x+y*W] = (x^y)%255;
 
 	// DMA operatins work directly on memory.
 	// Therefore, we need to insert a cache flush here so that
@@ -32,16 +33,23 @@ int main()
 
 	// Kick a DMA copy to move buffer A contents onto buffer B
 	const uint32_t blockCountInMultiplesOf16bytes = (W*H)/16;
-	UARTWrite("Initiating copy of ");
+	UARTWrite("Initiating copy loop of ");
 	UARTWriteDecimal(blockCountInMultiplesOf16bytes);
 	UARTWrite(" blocks\n");
-	DMACopyBlocks((uint32_t)bufferA, (uint32_t)bufferB, blockCountInMultiplesOf16bytes);
 
-	// This is not necessary, but used here as an indicator of copy being asynchronous
-	// and that we can still work while it's running.
-	while (DMAPending())
-		UARTWrite(".");
+	int32_t offset = 0;
+	int32_t dir = 1;
+	while (1)
+	{
+		// Copy at different lines to make the output appear to scroll
+		DMACopyBlocks((uint32_t)(bufferA+offset*W), (uint32_t)bufferB, blockCountInMultiplesOf16bytes);
 
-	UARTWrite("\nCopy is now done\n");
+		// Scroll
+		offset = (offset+dir)%H;
+		if (offset == 0 || offset == H-1) dir = -dir;
+
+		// This is not necessary, but used here as a way to avoid flicker
+		while (DMAPending()) { asm volatile("nop;"); }
+	}
 	return 0;
 }
