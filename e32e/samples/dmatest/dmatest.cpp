61902c1cd7b4b6a5e18e43d67eb0fb586ed7b7ab
Engin Cilasun <ecilasun@me.com>
2023-01-21 17:20:37 -0800

    More fixes to dma test

--- e32e/samples/dmatest/dmatest.cpp
+++ e32e/samples/dmatest/dmatest.cpp
@@ -7,22 +7,22 @@ int main()
 {
 	UARTWrite("DMA test\n");
 
-	const uint32_t W = 320;
-	const uint32_t H = 240;
-	const uint32_t mode = VIDEOMODE_320PALETTED;
+	const uint32_t W = 640;
+	const uint32_t H = 480;
+	const uint32_t mode = VIDEOMODE_640PALETTED;
 
 	UARTWrite("Preparing buffers\n");
 
 	// Create source and target buffers (using GPU functions to get aligned buffer addresses)
 	uint8_t *bufferB = GPUAllocateBuffer(W*H);
-	uint8_t *bufferA = GPUAllocateBuffer(W*H*2);
+	uint8_t *bufferA = GPUAllocateBuffer(W*H*3);
 
 	// Set buffer B as output
 	GPUSetVPage((uint32_t)bufferB);
 	GPUSetVMode(MAKEVMODEINFO(mode, VIDEOOUT_ENABLED)); // Mode 0, video on
 
 	// Fill buffer A with some data
-	for (uint32_t y=0;y<H*2;++y)
+	for (uint32_t y=0;y<H*3;++y)
 		for (uint32_t x=0;x<W;++x)
 			bufferA[x+y*W] = (x^y)%255;
 
@@ -38,7 +38,7 @@ int main()
 	UARTWrite(" blocks\n");
 
 	int32_t offset = 0;
-	int32_t dir = 1;
+	int32_t dir = 2;
 	int32_t averagetime = 131072;
 	int32_t outstats = 0;
 	while (1)
@@ -48,11 +48,7 @@ int main()
 		// Copy at different lines to make the output appear to scroll
 		DMACopyBlocks((uint32_t)(bufferA+offset*W), (uint32_t)bufferB, blockCountInMultiplesOf16bytes);
 
-		// Scroll
-		offset = (offset+dir)%H;
-		if (offset == 0 || offset == H-1) dir = -dir;
-
-		// This is not necessary, but used here as a way to avoid flicker
+		// Wait until there are no more DMA operations in flight
 		while (DMAPending()) { asm volatile("nop;"); }
 
 		uint64_t endtime = E32ReadTime();
@@ -65,6 +61,10 @@ int main()
 			UARTWrite("\n");
 		}
 		++outstats;
+
+		// Handle Scroll
+		offset = (offset+dir);
+		if (offset == 0 || offset == H*2-2) dir = -dir;
 	}
 	return 0;
 }
