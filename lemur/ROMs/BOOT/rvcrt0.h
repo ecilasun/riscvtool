19e0d425747cb268bf8f1a25f1084dd28fe7c89c
Engin Cilasun <ecilasun@me.com>
2023-01-29 15:06:42 -0800

    Updated ROM image to run tinyraytracertty

--- lemur/ROMs/BOOT/rvcrt0.h
+++ lemur/ROMs/BOOT/rvcrt0.h
@@ -16,7 +16,7 @@ extern "C"
          //"la gp, __global_pointer$;"
          //".option pop;"
 
-         "li sp, 0x00003FF0;"       // Stack is at near end of SDRAM (space at end for hardware devices)
+         "li sp, 0x0000FFF0;"       // Stack is at near end of SDRAM
 
 #if defined(MULTIHART_SUPPORT)
          // Set up stack spaces automatically when supporting
