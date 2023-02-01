3e59b50dddf4ed3b12f128317f2a54321dcad405
Engin Cilasun <ecilasun@me.com>
2023-01-31 19:40:14 -0800

    Small fix to ROM/stack

--- lemur/ROMs/BOOT/rvcrt0.h
+++ lemur/ROMs/BOOT/rvcrt0.h
@@ -16,7 +16,7 @@ extern "C"
          //"la gp, __global_pointer$;"
          //".option pop;"
 
-         "li sp, 0x0000FFF0;"       // Stack is at near end of SDRAM
+         "li sp, 0x0000FFF8;"       // Stack is at near end of SDRAM
 
 #if defined(MULTIHART_SUPPORT)
          // Set up stack spaces automatically when supporting
