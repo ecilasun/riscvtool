eb2fdcb730b693547be51c08dba7a74c46d3c762
Engin Cilasun <ecilasun@me.com>
2023-02-21 12:40:04 -0800

    Added sdcard sdk file Added context to gpu library

--- tinysys/SDK/gpu.h
+++ tinysys/SDK/gpu.h
@@ -9,26 +9,39 @@
 #define GPUCMD_SETPAL      0x00000001
 #define GPUCMD_SETVMODE    0x00000002
 
-#define VIDEOMODE_320PALETTED 0x0
-#define VIDEOMODE_640PALETTED 0x1
+enum EVideoMode
+{
+    EVM_320_Pal,
+    EVM_640_Pal,
+    EVM_Count
+};
 
-#define VIDEOOUT_DISABLED 0x0
-#define VIDEOOUT_ENABLED 0x1
+enum EVideoScanoutEnable
+{
+    EVS_Disable,
+    EVS_Enable,
+    EVS_Count
+};
 
-#define MAKECOLORRGB24(_r, _g, _b) (((_g&0xFF)<<16) | ((_r&0xFF)<<8) | (_b&0xFF))
-
-// Mode is 2 bits, scanenable is 1 bit
-#define MAKEVMODEINFO(_mode, _scanEnable) ((_mode&0x3)<<1) | (_scanEnable&0x1)
+struct EVideoContext
+{
+    enum EVideoMode m_mode;
+    enum EVideoScanoutEnable m_scanEnable;
+    uint32_t m_strideInWords;
+    uint32_t m_scanoutAddressCacheAligned;
+    uint32_t m_cpuWriteAddressCacheAligned;
+};
 
 // Utilities
 uint8_t *GPUAllocateBuffer(const uint32_t _size);
 
 // GPU side
-void GPUSetVMode(const uint32_t _vmodeInfo);
-void GPUSetVPage(const uint32_t _scanOutAddress64ByteAligned);
-void GPUSetPal(const uint8_t _paletteIndex, const uint32_t _rgba24);
+void GPUSetVMode(struct EVideoContext *_context, const enum EVideoMode _mode, const enum EVideoScanoutEnable _scanEnable);
+void GPUSetScanoutAddress(struct EVideoContext *_context, const uint32_t _scanOutAddress64ByteAligned);
+void GPUSetWriteAddress(struct EVideoContext *_context, const uint32_t _cpuWriteAddress64ByteAligned);
+void GPUSetPal(const uint8_t _paletteIndex, const uint32_t _red, const uint32_t _green, const uint32_t _blue);
 uint32_t GPUReadVBlankCounter();
 
 // Software emulated
-void GPUPrintString(uint8_t *_vramBase, const uint32_t _stride, const int _col, const int _row, const char *_message, int _length);
-void GPUClearScreen(uint8_t *_vramBase, const uint8_t _mode, const uint32_t _colorWord);
+void GPUPrintString(struct EVideoContext *_context, const int _col, const int _row, const char *_message, int _length);
+void GPUClearScreen(struct EVideoContext *_context, const uint32_t _colorWord);
