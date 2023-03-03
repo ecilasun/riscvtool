a8c958c0feed674786e2e60885cab6d8ce949888
Engin Cilasun <ecilasun@me.com>
2023-03-03 14:07:13 -0800

    Added DMA header to SDK

---
+++ tinysys/SDK/dma.c
@@ -0,0 +1,23 @@
+#include "dma.h"
+#include "core.h"
+
+// NOTE: Writes to this address will end up in the DMA command FIFO
+// NOTE: Reads from this address will return DMA operation status
+volatile uint32_t *DMAIO = (volatile uint32_t* )0x80006000;
+
+// Simple DMA with aligned start and target addresses, does _16byteBlockCount x 128bit copies (no masking, no byte aligned copies)
+void DMACopyBlocks(const uint32_t _sourceAddress16ByteAligned, const uint32_t _targetAddress16ByteAligned, const uint32_t _blockCountInMultiplesOf16bytes)
+{
+    *DMAIO = DMACMD_SETSOURCE;
+    *DMAIO = _sourceAddress16ByteAligned;
+    *DMAIO = DMACMD_SETTARGET;
+    *DMAIO = _targetAddress16ByteAligned;
+    *DMAIO = DMACMD_SETLENGHT;
+    *DMAIO = _blockCountInMultiplesOf16bytes;
+    *DMAIO = DMACMD_ENQUEUE;
+}
+
+uint32_t DMAPending()
+{
+    return *DMAIO;
+}
