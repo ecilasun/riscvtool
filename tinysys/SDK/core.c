ac10bb952eecb992f59b1eb9fe4a9943ffb0da6f
Engin Cilasun <ecilasun@me.com>
2023-03-31 22:16:41 -0700

    Added cache align

--- tinysys/SDK/core.c
+++ tinysys/SDK/core.c
@@ -7,8 +7,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/stat.h>
-
-#define STDOUT_FILENO 1
+//#include <unistd.h>
 
 #if defined(BUILDING_ROM)
 
@@ -35,8 +34,8 @@ uint32_t core_brk(uint32_t brkptr)
 	if (brkptr == 0)
 		return (uint32_t)__breakpos;
 
-	// NOTE: The value of the argument to brk() is rounded up for alignment with eight-byte boundaries
-	uint32_t alignedbrk = E32AlignUp(brkptr, 8);
+	// NOTE: The break address is aligned to the next multiple of 4 bytes
+	uint32_t alignedbrk = E32AlignUp(brkptr, 4);
 
 	// Out of bounds will return all ones (-1)
 	if (alignedbrk<(uint32_t)__heap_start || alignedbrk>(uint32_t)__heap_end)
@@ -57,6 +56,180 @@ uint32_t core_brk(uint32_t brkptr)
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+	/*int _chdir(const char *path)
+	{
+		errno = ENOSYS;
+		return -1;
+	}
+
+	char *_getcwd(char *buf, size_t size)
+	{
+		errno = -ENOSYS;
+		return NULL;
+	}
+
+	int _fstat(int fd, struct stat *statbuf)
+	{
+		register uint32_t fdsc = (uint32_t)fd;
+		register uint32_t fsta = (uint32_t)statbuf;
+		register int retval = 0;
+		asm (
+			"li a7, 62;"
+			"mv a0, %1;"
+			"mv a1, %2;"
+			"ecall;"
+			"mv %0, a0;" :
+			// Return values
+			"=r" (retval) :
+			// Input parameters
+			"r" (fdsc), "r" (fsta) :
+			// Clobber list
+			"a0", "a1", "a7"
+		);
+		return retval;
+	}
+
+	int _isatty(int file)
+	{
+		return (file == STDOUT_FILENO);
+	}
+
+	int _close(int file)
+	{
+		register uint32_t fd = (uint32_t)file;
+		register int retval = 0;
+		asm (
+			"li a7, 57;"
+			"mv a0, %1;"
+			"ecall;"
+			"mv %0, a0;" :
+			// Return values
+			"=r" (retval) :
+			// Input parameters
+			"r" (fd) :
+			// Clobber list
+			"a0", "a7"
+		);
+		return retval;
+	}
+
+	off_t _lseek(int file, off_t ptr, int dir)
+	{
+		register uint32_t fd = (uint32_t)file;
+		register uint32_t fptr = (uint32_t)ptr;
+		register uint32_t fdir = (uint32_t)dir;
+		register int retval = 0;
+		asm (
+			"li a7, 62;"
+			"mv a0, %1;"
+			"mv a1, %2;"
+			"mv a2, %3;"
+			"ecall;"
+			"mv %0, a0;" :
+			// Return values
+			"=r" (retval) :
+			// Input parameters
+			"r" (fd), "r" (fptr), "r" (fdir) :
+			// Clobber list
+			"a0", "a1", "a2", "a7"
+		);
+		return retval;
+	}
+
+	int _lstat(const char *file, struct stat *st)
+	{
+		//st->st_mode = S_IFCHR; // S_IFBLK for disk data?
+		return 0;
+	}
+
+	int _open(const char *name, int flags, int mode)
+	{
+		register uint32_t fnam = (uint32_t)name;
+		register uint32_t flg = (uint32_t)flags;
+		register uint32_t fmod = (uint32_t)mode;
+		register int retval = 0;
+		asm (
+			"li a7, 1024;"
+			"mv a0, %1;"
+			"mv a1, %2;"
+			"mv a2, %3;"
+			"ecall;"
+			"mv %0, a0;" :
+			// Return values
+			"=r" (retval) :
+			// Input parameters
+			"r" (fnam), "r" (flg), "r" (fmod) :
+			// Clobber list
+			"a0", "a1", "a2", "a7"
+		);
+		return retval;
+	}
+
+	int _openat(int dirfd, const char *name, int flags, int mode)
+	{
+		// https://linux.die.net/man/2/openat
+		errno = ENOSYS;
+		return -1;
+	}
+
+	int _stat(const char *file, struct stat *st)
+	{
+		//st->st_mode = S_IFCHR; // S_IFBLK for disk data?
+		return 0;
+	}
+
+	ssize_t _read(int file, void *ptr, size_t len)
+	{
+		register uint32_t fd = (uint32_t)file;
+		register uint32_t fptr = (uint32_t)ptr;
+		register uint32_t flen = (uint32_t)len;
+		register int retval = 0;
+		asm (
+			"li a7, 63;"
+			"mv a0, %1;"
+			"mv a1, %2;"
+			"mv a2, %3;"
+			"ecall;"
+			"mv %0, a0;" :
+			// Return values
+			"=r" (retval) :
+			// Input parameters
+			"r" (fd), "r" (fptr), "r" (flen) :
+			// Clobber list
+			"a0", "a1", "a2", "a7"
+		);
+		return retval;
+	}
+
+	ssize_t _write(int file, const void *ptr, size_t len)
+	{
+		register uint32_t fd = (uint32_t)file;
+		register uint32_t fptr = (uint32_t)ptr;
+		register uint32_t flen = (uint32_t)len;
+		register int retval = 0;
+		asm (
+			"li a7, 64;"
+			"mv a0, %1;"
+			"mv a1, %2;"
+			"mv a2, %3;"
+			"ecall;"
+			"mv %0, a0;" :
+			// Return values
+			"=r" (retval) :
+			// Input parameters
+			"r" (fd), "r" (fptr), "r" (flen) :
+			// Clobber list
+			"a0", "a1", "a2", "a7"
+		);
+		return retval;
+	}
+
+	int _wait(int *status)
+	{
+		errno = ECHILD;
+		return -1;
+	}*/
 
 	int _brk(void *addr)
 	{
