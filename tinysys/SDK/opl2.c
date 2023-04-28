d5c744881f996f521ae930e437c20d27cffa81cd
Engin Cilasun <ecilasun@me.com>
2023-04-28 01:42:37 -0700

    OPL2 commands have to wait for latch

--- tinysys/SDK/opl2.c
+++ tinysys/SDK/opl2.c
@@ -1,4 +1,5 @@
 #include "opl2.h"
+#include "basesystem.h"
 
 volatile uint8_t *IO_OPL2REG = (volatile uint8_t*)0x80009000;
 volatile uint8_t *IO_OPL2VAL = (volatile uint8_t*)0x80009004;
@@ -6,9 +7,11 @@ volatile uint8_t *IO_OPL2VAL = (volatile uint8_t*)0x80009004;
 void OPL2WriteReg(uint8_t reg)
 {
     *IO_OPL2REG = reg;
+	E32Sleep(200);	// Need to give time for internal processing
 }
 
 void OPL2WriteVal(uint8_t val)
 {
 	*IO_OPL2VAL = val;
+	E32Sleep(200);	// Need to give time for internal processing
 }
